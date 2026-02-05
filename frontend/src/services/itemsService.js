9fe5b058b17ae437b0337eebd5d846396a5bbd1e
Pael <patel18611@gmail.com>
2026-02-05 10:53:52 +0530

    Fixed Netlify MIME error

--- frontend/src/services/itemsService.js
+++ frontend/src/services/itemsService.js
@@ -1,5 +1,5 @@
 export const addItemToServer = async (task, date) => {
-  const response = await fetch("https://github.com/patel18-patel/todo-app", {
+  const response = await fetch("http://localhost:3000/api/todo", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -11,14 +11,14 @@ export const addItemToServer = async (task, date) => {
 };
 
 export const getItemsFromServer = async () => {
-  const response = await fetch("https://github.com/patel18-patel/todo-app");
+  const response = await fetch("http://localhost:3000/api/todo");
   const items = await response.json();
   return items.map(mapServerItemToLocalItem);
 };
 
 export const markItemCompletedOnServer = async (id) => {
   const response = await fetch(
-    `https://github.com/patel18-patel/todo-app/${id}/completed`,
+    `http://localhost:3000/api/todo/${id}/completed`,
     {
       method: "PUT",
     },
@@ -28,7 +28,7 @@ export const markItemCompletedOnServer = async (id) => {
 };
 
 export const deleteItemFromServer = async (id) => {
-  await fetch(`https://github.com/patel18-patel/todo-app/${id}`, {
+  await fetch(`http://localhost:3000/api/todo/${id}`, {
     method: "DELETE",
   });
   return id;
